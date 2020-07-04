102047710db1c6b9feab6612c1aaeda23714e926
Andreas Borgstad <aborgstad@gmail.com>
2020-07-04 19:11:07 +0200

    test(linked list): add test for linked list: init/add/delete API

--- Makefile
+++ Makefile
@@ -1,13 +1,14 @@
 CC=gcc
 CFLAGS=-I. -g
-DEPS = btree.h io.h hash.h
-OBJ = btree.o main.o io.o hash.o
+DEPS = btree.h io.h hash.h linked_list.h
+OBJ = btree.o main.o io.o hash.o linked_list.o
 
 TEST_HASH = tests/hash_test.o
 TEST_BTREE = tests/btree_test.o
 TEST_IO = tests/io_test.o
+TEST_LINKED_LIST = tests/linked_list_test.o
 
-TEST_DEP = btree.o io.o hash.o
+TEST_DEP = btree.o io.o hash.o linked_list.o
 
 
 %.o: %.c $(DEPS)
@@ -28,8 +29,9 @@ test_btree: $(TEST_BTREE) $(TEST_DEP)
 test_hash: $(TEST_HASH) $(TEST_DEP)	
 	$(CC) $(TEST_HASH) $(TEST_DEP) -o $@ && ./test_hash
 
+test_linked_list: $(TEST_LINKED_LIST) $(TEST_DEP)	
+	$(CC) $(TEST_LINKED_LIST) $(TEST_DEP) -o $@ && ./test_linked_list
+
 .PHONY: test_io
 test_io: $(TEST_IO) $(TEST_DEP)	
 	$(CC) $(TEST_IO) $(TEST_DEP) -o $@ && ./test_io
-
-
---
+++ tests/linked_list_test.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "linked_list.h"
+
+LinkedList *getLinkedList(int size)
+{
+  LinkedList *l = initializeLinkedList((Id)0);
+  for (int i = 1; i < size; i++)
+    addLinkedList(l, (Id)i);
+  return l;
+}
+
+void testLinkedListInsert(int iterations)
+{
+  LinkedList *l = getLinkedList(iterations);
+  for (int i = 0; i < iterations; i++)
+  {
+    assert((Id)i == l->data);
+    l = l->next;
+  }
+}
+
+void testLinkedListDelete(int iterations)
+{
+  LinkedList *list = getLinkedList(iterations);
+  int itemToDelete[] = {1, 3, 5};
+  for (int i = 0; i < 3; i++)
+  {
+    deleteLinkedList(list, itemToDelete[i]);
+  }
+  while (list->next)
+  {
+
+    for (int i = 0; i < 3; i++)
+    {
+      assert(itemToDelete[i] != list->data);
+    }
+    list = list->next;
+  }
+}
+
+int main()
+{
+  testLinkedListInsert(5);
+  testLinkedListDelete(10);
+}
