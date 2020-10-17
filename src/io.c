0bcd10af02f05e137d2b10af28e5c3971984047d
Andreas Borgstad <aborgstad@gmail.com>
2020-10-17 13:00:30 +0200

    docs(io): add initial docstring

--- src/io.c
+++ src/io.c
@@ -7,6 +7,16 @@ static Cache *cache;
 // TODO: set dynamically
 static int maxNodesInMem;
 
+/**
+ * ioInitialize - initialization of IO module
+ *
+ * Entry function for initializing IO module, used by the btree. This module
+ * initializes a storage- and cache module. The storage module supports disk reads,
+ * the cache is a simple in-memory FIFO, whose size is statically set in this module.
+ * The IO operations are performed on individual nodes, and the result is always
+ * materialized in the in-memory cache. It is only if the cache is fully, that the
+ * oldest element is written to disk, and removed from the cache.
+ */
 void ioInitialize()
 {
     initializeStorage();
@@ -14,6 +24,16 @@ void ioInitialize()
     maxNodesInMem = 1024;
 }
 
+/**
+ * ioRead - main read abstraction used by the btree for reading nodes
+ * 
+ * @id: id to read
+ * @maxDegree: max keys in node.
+ *
+ * The id is assumed to be unique, and first the cache is searched for the item.
+ * If the item is in the cache, it is returned. If not, then it is read from
+ * disk and insert into the cache.
+ */
 Node *ioRead(BlockId id, int maxDegree)
 {
     Node *node = cacheGet(cache, id);
@@ -41,6 +61,16 @@ Node *ioRead(BlockId id, int maxDegree)
     }
 }
 
+/**
+ * ioWrite - main write abstraction used by the btree for writing nodes
+ * 
+ * @node: the node to write
+ * @id: id of node to write
+ * @maxDegree: max keys in node
+ * 
+ * Adds the node to cache. If the node is already there, the cache is updated. If the
+ * cache is full, replace the oldest element, and insert the newest.
+ */
 void *ioWrite(Node *node, BlockId id, int maxDegree)
 {
     // if the cache is not full, either update or add entry
@@ -67,12 +97,26 @@ void *ioWrite(Node *node, BlockId id, int maxDegree)
     }
 }
 
+/**
+ * ioCacheAdd - adds an element to the cache. Assumes the cache is not full
+ * 
+ * @id: id of node
+ * @node: pointer to node
+ *
+ */
 void *ioCacheAdd(BlockId id, Node *node)
 {
     cacheAdd(cache, id, node);
     cache->nodesInMem++;
 }
 
+/**
+ * ioCacheReplace - replaces the oldest element from the cahce, and inserts new element
+ * 
+ * @id: id of node to insert
+ * @maxDegree: max number of keys for node
+ * @node: node to insert
+ */
 void *ioCacheReplace(BlockId id, int maxDegree, Node *node)
 {
     BlockId toWriteBlockId = cache->lru->blockId;
@@ -86,6 +130,10 @@ void *ioCacheReplace(BlockId id, int maxDegree, Node *node)
     ioCacheAdd(id, node);
 }
 
+/** ioFlush - wrapper function to flush the cache, i.e., write everything to disk
+ * 
+ * @maxDegree: max number of keys for nodes
+ */
 void ioFlush(int maxDegree)
 {
     cacheFlush(cache, maxDegree);
